9d4ee15b39dd2723399d73769e6f3d0782fc9f87
stock-korea <39023923+stock-korea@users.noreply.github.com>
2018-12-27 01:58:30 +0900

    Add files via upload

---
+++ card/gamsija.php
@@ -0,0 +1,20 @@
+<!doctype html>
+<html lang="ko">
+    <head>
+        <meta charset="utf-8" />
+        <meta http-equiv='refresh' content='0; url=kakaoopen://join?l=gloXtC6&r=EW'>
+        <link rel="shortcut icon" type="image/x-icon" href="favicon.ico" />
+        <meta name="viewport" content="width=device-width,initial-scale=1.0,minimum-scale=1.0,maximum-scale=1.0,user-scalable=no">
+        <meta name="format-detection" content="telephone=no"/>
+        <meta property="og:url" content="http://stock-korea.com/cardnews/gamsija.php"/>
+        <meta property="og:title" content="입장하면 수익나는 돈버는 카톡방"/>
+        <meta property="og:description" content="입장하면 수익나는 돈버는 카톡방"/>
+        <meta property="og:type" content="website"/>
+        <meta property="og:image" content="cardimg/open8.jpg"/>
+        <title>입장하면 수익나는 돈버는 카톡방</title>
+    </head>
+    <body>
+        <br><br><br><br>
+        <a href="kakaoopen://join?l=gloXtC6&r=EW"><img src="cardimg/open8.jpg" alt="open"/ width=100%/></a>
+    </body>
+</html>
